6991f61a7ebd3d3eef0b26233b0f0c6f96707818
Matheus <matheus@flowsaas.com>
2026-03-06 13:01:41 -0300

    fix: remove root test files breaking nextjs build

--- check-orgs.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
-
-async function main() {
-    const orgs = await prisma.organization.findMany()
-    console.log(JSON.stringify(orgs, null, 2))
-}
-
-main().finally(() => prisma.$disconnect())
